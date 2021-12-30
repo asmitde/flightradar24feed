5866f3aabbdb10a46b823df37629163dabedcf31
Asmit De <asmit.de@outlook.com>
2021-12-30 06:34:09 -0800

    Updated run script with network and config options

--- fr24feed.sh
+++ fr24feed.sh
@@ -1,20 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+# Run this optionally with param signup: e.g., ./fr24feed.sh signup
 
 LOGPATH=/mnt/usbnas0/appdata/fr24feed/log
 CONFIGDIR=/mnt/usbnas0/appdata/fr24feed/config
-CONFIGFILE=fr24feed.ini
-IMAGE=fr24feed:test
+
+IMAGE=fr24feed:latest
 CONTAINER=fr24feed
+NETWORK=adsb-net
 
 # Stop and remove running readsb containers
-docker stop $CONTAINER
-docker rm $CONTAINER
+docker stop $(docker ps -f name=$CONTAINER -q)
+docker rm $(docker ps -f name=$CONTAINER -f status=exited -q)
 
-docker run \
- -d \
- --name $CONTAINER \
- --hostname $CONTAINER \
- -p 8754:8754 \
- -v $CONFIGDIR:/app/fr24feed_armhf/config
- -v $LOGPATH:/var/log/fr24feed \
- $IMAGE
+# Create a network to receice adsb data from sdrs
+[[ -z $(docker network ls -f name=$NETWORK -q) ]] && docker network create $NETWORK
+
+# Run the container
+case $1 in
+  signup)
+    docker run \
+      -it \
+      --rm \
+      --name $CONTAINER \
+      $IMAGE \
+      --signup
+  ;;
+  *)
+    docker run \
+      -d \
+      --restart on-failure:5 \
+      --name $CONTAINER \
+      --hostname $CONTAINER-client \
+      --network $NETWORK \
+      -p 8754:8754 \
+      -v $CONFIGDIR:/app/config \
+      -v $LOGPATH:/var/log/fr24feed \
+      $IMAGE
+esac
